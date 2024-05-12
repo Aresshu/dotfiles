0f03f4a5c0ed27a3c76e118b9be843abf537e59e
Aresshu <andresdordonez@proton.me>
2024-05-11 22:49:25 -0400

    refactor: nvim-lspconfig

--- .config/nvim/lua/alesh/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/alesh/plugins/lsp/lspconfig.lua
@@ -1,9 +1,10 @@
 return {
     "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
 
     config = function()
         local lspconfig = require("lspconfig")
-
+        local keymap = vim.keymap
         -- Rust_analyzer
         lspconfig.rust_analyzer.setup ({})
 
@@ -17,11 +18,28 @@ return {
         lspconfig.gopls.setup({})
 
         -- Keymaps
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+            callback = function(ev)
+                local opts = { buffer = ev.buf, silent = true }
+                keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+                keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+                keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+                keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+                keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+                keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+                keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+                keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+            end
+        })
+
+        local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+        for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+        end
     end,
 }
 
