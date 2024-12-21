7eb2d0f06a1bbba2a84b2052c22583465fc4a1d2
Aresshu <andresdordonez@proton.me>
2024-12-21 00:23:29 -0500

    ui changes: colorscheme

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -24,9 +24,6 @@ require("lazy").setup({
   ui = {
     border = "single"
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "kanagawa" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,40 +1,51 @@
 return {
-  'rebelot/kanagawa.nvim',
+  -- 'rebelot/kanagawa.nvim',
+  -- lazy = false,
+  -- priority = 1000,
+  -- config = function()
+  --   require('kanagawa').setup({
+  --     transparent = false,
+  --     theme = "dragon",
+  --     colors = {
+  --       theme = {
+  --         all = {
+  --           ui = {
+  --             bg_gutter = "none"
+  --           }
+  --         }
+  --       }
+  --     },
+  --
+  --     overrides = function(colors)
+  --       local theme = colors.theme
+  --       return {
+  --         NormalFloat = { bg = "none" },
+  --         FloatBorder = { bg = "none" },
+  --         FloatTitle = { bg = "none" },
+  --
+  --         TelescopeTitle = { fg = theme.ui.special, bold = true },
+  --         TelescopePromptNormal = { bg = "none" },
+  --         TelescopePromptBorder = { bg = "none" },
+  --         TelescopeResultsNormal = { bg = "none" },
+  --         TelescopeResultsBorder = { bg = "none" },
+  --         TelescopePreviewNormal = { bg = "none" },
+  --         TelescopePreviewBorder = { bg = "none" },
+  --       }
+  --     end,
+  --
+  --   })
+  --   vim.cmd.colorscheme('kanagawa')
+  -- end
+  "ellisonleao/gruvbox.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-
-    require('kanagawa').setup({
-      transparent = true,
-      theme = "dragon",
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none"
-            }
-          }
-        }
-      },
-
-      overrides = function(colors)
-        local theme = colors.theme
-        return {
-          NormalFloat = { bg = "none" },
-          FloatBorder = { bg = "none" },
-          FloatTitle = { bg = "none" },
-
-          TelescopeTitle = { fg = theme.ui.special, bold = true },
-          TelescopePromptNormal = { bg = "none" },
-          TelescopePromptBorder = { bg = "none" },
-          TelescopeResultsNormal = { bg = "none" },
-          TelescopeResultsBorder = { bg = "none" },
-          TelescopePreviewNormal = { bg = "none" },
-          TelescopePreviewBorder = { bg = "none" },
-        }
-      end,
-
+    require("gruvbox").setup({
+      terminal_colors = true,
+      transparent_mode = true,
     })
-    vim.cmd.colorscheme('kanagawa')
+    vim.o.background = "dark"
+    vim.cmd("colorscheme gruvbox")
   end
 }
+
