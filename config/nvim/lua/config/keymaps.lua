3c7b510e1c307676c83887eebea001a5cf75d4b2
Aresshu <andresdordonez@proton.me>
2024-12-30 00:54:59 -0500

    Change theme

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -5,6 +5,9 @@ local telescope_builtin = require('telescope.builtin')
 -- Clear highlights when searching
 map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Write file 
+map('n', '<leader>w', '<cmd>:w<CR>')
+
 -- Diagnostics
 map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics quickfix list' })
 
--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,25 @@
 return {
-  "ellisonleao/gruvbox.nvim",
-  lazy = false,
+  "aktersnurra/no-clown-fiesta.nvim",
   priority = 1000,
+  lazy = false,
   config = function()
-    require("gruvbox").setup({
-      terminal_colors = true,
-      transparent_mode = true,
+    require('no-clown-fiesta').setup({
+      transparent = true,
+      styles = {},
     })
-    vim.o.background = "dark"
-    vim.cmd("colorscheme gruvbox")
-  end
+    vim.cmd('colorscheme no-clown-fiesta')
+  end,
 }
+
+-- "ellisonleao/gruvbox.nvim",
+  -- lazy = false,
+  -- priority = 1000,
+  -- config = function()
+  --   require("gruvbox").setup({
+  --     terminal_colors = true,
+  --     transparent_mode = true,
+  --   })
+  --   vim.o.background = "dark"
+  --   vim.cmd("colorscheme gruvbox")
+  -- end
+
